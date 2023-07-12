c281035ccbc6ec11a5d288cfea3b6e078e746c81
punjabinuclei <guppi0204@gmail.com>
2023-07-12 14:38:28 +0530

    HomepageUi99%Done

--- src/components/SignUpPage/components/Form/Form.tsx
+++ src/components/SignUpPage/components/Form/Form.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 import GoogleIcon from '../../../../Assets/Icons/google-icon 1.svg'
 import AppleIcon from '../../../../Assets/Icons/apple-1.svg'
-import { Lato } from 'next/font/google'
+// import { Lato } from 'next/font/google'
 
 // const lato = Lato({
 //     subsets: ['latin'],
+//     weight: ['100', '300', '400', '700'],
 // });
 
 
@@ -20,30 +21,42 @@ const Form = () => {
 
                 <div className='flex mt-[1.63rem]'>
                     <div className='mr-[1.16rem]'>
-                        <button className='pl-[1.63rem] pr-[.19rem] flex items-center w-[11.35rem] h-[1.875rem] bg-white text-[#858585] text-[0.75rem] rounded-xl' ><Image className='mr-[0.63rem]' src={GoogleIcon} alt="googleIcon" /> Sign in with Google</button>
+                        <button className='pl-[1.45rem] pr-[.19rem] flex items-center w-[11.35rem] h-[1.875rem] bg-white text-[#858585] text-[0.75rem] rounded-[0.63rem]' ><Image className='mr-[0.63rem]' src={GoogleIcon} alt="googleIcon" /> Sign in with Google</button>
                     </div>
                     <div>
-                        <button className='pl-[1.63rem] flex items-center w-[11.25rem] h-[1.875rem] bg-white text-[#858585] text-[0.75rem] rounded-xl'><Image className='mr-[0.63rem]' src={AppleIcon} alt="appleIcon" />Sign in with Apple</button>
+                        <button className='pl-[1.45rem] flex items-center w-[11.25rem] h-[1.875rem] bg-white text-[#858585] text-[0.75rem] rounded-[0.63rem]'><Image className='mr-[0.63rem]' src={AppleIcon} alt="appleIcon" />Sign in with Apple</button>
                     </div>
                 </div>
-                <div className='w-[24.0625rem] h-[19.8125rem] rounded-[1.25rem] bg-white mt-[1.56rem]'>
-                    <div>
-                        <p className='text-[1rem]'>Email address</p>
-                        <input placeholder='johndoe@gmail.com' type='email' className='rounded-[0.63rem] w-[20.3rem] h-[2.5rem] flex items-center text-[1rem]' />
+                <div className='w-[24.0625rem] h-[20.5rem] rounded-[1.25rem] bg-white mt-[1.56rem]'>
+                    <div >
+                        <div>
+                            <p className='text-[1rem] pt-[1.88rem] pl-[1.88rem] pb-[0.62rem] font-[500]'>Email address</p>
+                            <input
+                                autoComplete='off'
+                                style={{ outline: 'none' }}
+                                placeholder='johndoe@gmail.com'
+                                type='email'
+                                className='pl-[0.94rem] mx-auto rounded-[0.63rem] w-[20.3rem] h-[2.5rem] flex items-center text-[1rem] bg-[#F5F5F5]' />
+                        </div>
+                        <div>
+                            <p className='text-[1rem] pt-[1.25rem]  pl-[1.88rem] pb-[0.62rem] font-[500]'>Password</p>
+                            <input
+                                style={{ outline: 'none' }}
+                                autoComplete='off'
+                                placeholder='********'
+                                type='text'
+                                className='pl-[0.94rem] mx-auto rounded-[0.63rem] w-[20.3rem] h-[2.5rem] flex items-center text-[1rem] bg-[#F5F5F5]' />
+                        </div>
+                        <div>
+                            <button className='pt-[1.25rem] pl-[1.94rem] text-[#346BD4] font-medium' >Forgot password?</button>
+                        </div>
                     </div>
-                    <div>
-                        <p>Password</p>
-                        <input placeholder='********' type='password' className='' />
-                    </div>
-                    <div>
-                        <p>Forgot password?</p>
-                    </div>
-                    <div>
-                        <button>Sign In</button>
+                    <div className='flex justify-center '>
+                        <button className='w-[20.3125rem] h-[2.5rem] bg-black text-white text-[1rem] font-bold rounded-[0.63rem] mt-[1.25rem] '>Sign In</button>
                     </div>
                 </div>
-                <div>
-                    <p>Don't have an account? Register here </p>
+                <div className='mt-[1.25rem]'>
+                    <p className='text-[#858585] text-[1rem] text-center font-[500]'>Don't have an account?<span className='text-[#346BD4]'> Register here </span></p>
                 </div>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import SignUpPage from '@/components/SignUpPage/SignUpPage'
 const index = () => {
   return (
     <div>
+      <h1>Add lato text</h1>
       <SignUpPage />
     </div>
   )
