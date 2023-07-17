0f4ab9898ff82bc8b835ddce4c2303321470ec53
punjabinuclei <guppi0204@gmail.com>
2023-07-17 18:40:41 +0530

    AuthFix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,5 +8,5 @@ export default NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
 });
