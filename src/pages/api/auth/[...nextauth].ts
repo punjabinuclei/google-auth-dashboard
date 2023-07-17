3bb556993eb465822b3b5cb06061ea97cc6217d0
punjabinuclei <guppi0204@gmail.com>
2023-07-17 18:47:58 +0530

    AuthFixTest

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+
+
 export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  secret: process.env.NEXT_PUBLIC_SECRET,
+
+providers: [
+  GoogleProvider({
+    clientId:"599815915021-3u2bounbnc324u12liqpepvat4stpssl.apps.googleusercontent.com",
+    clientSecret:"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"
+  }),
+],
+secret:"mrfRIoixyUjdyfwOIp8TBR2Gz2dGoYCYmvcApWryKlI="
 });
+
+
